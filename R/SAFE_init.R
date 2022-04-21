ae888204b59fc505656b012daf1787f7bfc82cd4
Jon McCurdy <jonmccurdy@ymail.com>
2022-04-21 16:28:06 -0500

    Include SAFE and prevent corrupt states

--- R/SAFE_init.R
+++ R/SAFE_init.R
@@ -1,19 +1,19 @@
-#' DX_init
+#' SAFE_init
 #'
-#' @param K number of random numbers wanted
-#' @param S number of random numbers wanted
 #'
 #' @return
 #' @export
 #'
 #' @examples
-#' dx_init()
+#' SAFE_init()
 #' 
 
-dx_init <- function(K=47, S=1) {
+SAFE_init <- function(K_X=4, K_S=5) {
   if((RNGkind()!="user-supplied")[1]) RNGkind("user")
-  .Random.seed[2] <- K
-  .Random.seed[3] <- S
+  .Random.seed[2] <- 47
+  .Random.seed[3] <- 13
+  .Random.seed[4] <- K_X
+  .Random.seed[5] <- K_S
   .Random.seed <<- as.integer(.Random.seed)
   invisible(runif(1))
   set.seed(.Random.seed[6])
--- src/RNG-c-code.cpp
+++ src/RNG-c-code.cpp
@@ -7,6 +7,22 @@ using namespace Rcpp;
 #define K_MAX	3500
 #define PP 2147483647  //2^31-1
 #define IPP 4.6566129e-10
+#define ROTR(x,n) ((((unsigned long) x)>>(n))|(((unsigned long) x)<<(32-(n))))
+#define f1(x) (ROTR(x, 25))
+#define f2(x) (ROTR(x, 23))
+#define f3(x) (ROTR(x, 12))
+#define f4(x) (ROTR(x, 10))
+#define TSIZE 256 		// actual table size used
+#define maxT 1024 		//maximum table size
+#define maxB 15
+#define B_X 32896  		
+#define B_Y 32776
+#define T_MOD TSIZE-1
+#define MOD2_32 4294967295  
+static unsigned long  K[8], IV[8];   /*KEY and IV vectors*/
+static int TABLE_X[maxT], TABLE_Y[maxT];
+static int I_Y, BURN_X, BURN_Y;
+
 static long long B_X1 = 536869888; // will change based on K, S- can be done more efficiently
 static long long B_X2 = 65011712;
 static long long B_X3 = 67633152;
@@ -17,10 +33,12 @@ static int I_X;           /* running index */
 unsigned long MODP(unsigned long z) {return ((((z)&PP)+((z)>>31)) &PP);}
 
 static int K_X = 47;
+static int K_SX = 4;
+static int K_SY = 5;
 static int S_X = 1;
-static int nseed = 3;
-static Int32 seed[K_MAX];
-static int XX[K_MAX];
+static int nseed = 5;
+static int seed[K_MAX]={K_X,S_X,K_SX,K_SY};
+static int XX[K_MAX], YY[K_MAX];
 static double res;
 int set=0;
 
@@ -65,23 +83,85 @@ void dx_4(){
   res= (double) XX[I_X] * IPP;
 }
 
-#define B20(x) ( ((x)>>11) + ((x)<<20)&PP ) 
-#define B9(x) ( ((x)>>22) + ((x)<<9) &PP )
-unsigned long S ;
+/*  SAFE */
 
-void dx_2_fast(){
-  K_X = seed[0];
-  S_X = seed[1];
-  int II0 = I_X;
-  if(++I_X >= K_X)  I_X = 0;     /*wrap around running index */
-  S = MODP(XX[I_X] + XX[II0]);
-  XX[I_X] = MODP(B20(S) + B9(S));
-  res = (double) XX[I_X] * IPP;
+void X_RNG_INIT(){
+  int i;
+  srand((seed[1]+K[0]) & PP);
+  for (i=0; i<8; i++)
+    XX[i] = (rand()+K[i]+IV[8-i]) & PP; 
+  for (i=8; i<K_MAX; i++)
+    XX[i] = rand() & PP; 
+  I_X=K_SX-1;
 }
 
-void lcg_basic(){
-  seed[2] = seed[2] + 1;
-  res = seed[2] * 2.32830643653869e-10;
+int RNG_X(void){
+  int II0 = I_X;
+  if(++I_X >= K_SX)  I_X = 0;     /*wrap around running index */
+  XX[I_X] = MODP(B_X * XX[I_X] + XX[II0]);
+return XX[I_X];
+}
+
+void Y_RNG_INIT(){
+  int i;
+  srand((seed[1]+K[1]) & PP);
+  for (i=0; i<8; i++)
+    YY[i] = (rand()+K[8-i]+IV[i]) & PP;
+  for (i=8; i<K_MAX; i++)
+    YY[i] = rand() & PP; 
+  I_Y=K_SY-1;
+}
+
+int RNG_Y(void){
+  int II0 = I_Y;
+  if(++I_Y >= K_SY)  I_Y = 0;     /*wrap around running index */
+  YY[I_Y] = MODP(B_Y * YY[I_Y] + YY[II0]);
+  return YY[I_Y];
+}
+
+void init_SAFE_X_Y(void){
+  int i;
+  
+  srand(seed[4]);
+  for (i = 0; i < 8; i++){
+    K[i] = rand();
+    IV[i] = rand();
+  }
+  
+  X_RNG_INIT();
+  Y_RNG_INIT();
+  
+  BURN_X = (K[0]+ K[3] + IV[1] + IV[2]) & maxB;
+  BURN_Y = (K[1]+ K[2] + IV[0] + IV[3]) & maxB;
+  for(i=0; i<BURN_X; i++) RNG_X(); 
+  for(i=0; i<BURN_X; i++) RNG_Y(); 
+  
+  for(i=0; i<TSIZE; i++) 
+    TABLE_X[i] = RNG_X(); 
+  for(i=0; i<TSIZE; i++) 
+    TABLE_Y[i] = RNG_Y(); 
+}
+
+void SAFE_X_Y(void){
+  unsigned long x0, y0, vv, ww;
+  int X0, Y0;
+  x0 = RNG_X();
+  y0 = RNG_Y();
+  Y0=y0&T_MOD;
+  X0=x0&T_MOD;
+  
+  vv = TABLE_X[Y0]; TABLE_X[Y0]=x0;
+  ww = TABLE_Y[X0]; TABLE_Y[X0]=y0;
+  
+  res=(double)((f1(x0)+f2(y0)+f4(ww)) & MOD2_32)* IPP;
+}
+/*  SAFE */
+
+void dx_init(){
+  srand(seed[4]);
+  for (i=0; i< K_MAX; i++){
+    XX[i] = rand() & PP;
+  }
 }
 
 void (*dx_gen)();
@@ -89,21 +169,32 @@ void (*dx_gen)();
 void generator_type(){
   switch(S_X){
   case 1: dx_gen=&dx_1;
+    dx_init();
+    init_SAFE_X_Y();
     break;
   case 2: dx_gen=&dx_2;
+    dx_init();
+    init_SAFE_X_Y();
     break;
   case 3: dx_gen=&dx_3;
+    dx_init();
+    init_SAFE_X_Y();
     break;
   case 4: dx_gen=&dx_4;
+    dx_init();
+    init_SAFE_X_Y();
     break;
-  case 5: dx_gen=&dx_2_fast;
-    break;
-  case 6: dx_gen=&lcg_basic;
+  case 13: dx_gen=&SAFE_X_Y;
+    dx_init();
+    init_SAFE_X_Y();
     break;
   default: dx_gen=&dx_1;
-  Rcerr << "The value of 'S' that was chosen is not compatible with this package. \nBy default, a DX-" << K_X<< "-1 generator was chosen.\n";
-  S_X=1;
-  break;
+    dx_init();
+    init_SAFE_X_Y();
+    S_X=1;
+    seed[1]=S_X;
+    Rcerr << "The value of 'S' that was chosen is not compatible with this package. \nBy default, a DX-" << K_X<< "-1 generator was chosen.\n";
+    break;
   }
 }
 
@@ -113,14 +204,16 @@ double * user_unif_rand(){
 }
 
 void user_unif_init(Int32 seed_in) {
-  generator_type();
-  seed[0] = K_X;
-  seed[1] = S_X;
+  K_X = seed[0];
+  S_X = seed[1];
+  K_SX = seed[2];
+  K_SY = seed[3];
+  
   srand(seed_in);
-  seed[2] = rand() & PP;
-  for (i=0; i< seed[0]; i++){
-    XX[i] = rand() & PP;
-  } 
+  seed[4] = rand() & PP;
+  
+  generator_type();
+
   I_X = seed[0]-1;
   K12 = seed[0]/2-1;
   K13 = 2*seed[0]/3-1;   
